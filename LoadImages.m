a6094a55e3e1d562c7b87a90c4d33d16deee205d
cttltran <23510284+cttltran@users.noreply.github.com>
2018-05-23 08:14:51 +0200

    updated code for LoadImages

--- LoadImages.m
+++ LoadImages.m
@@ -1,29 +1,40 @@
-function [handles] = LoadImages(handles)
-%LOADIMAGES Summary of this function goes here
-%Detailed explanation goes here
+function MyData = loadImages(handles)
+%LOADIMAGES The function loads dicom files from a library chosen by the
+%user
+% The function limits the possible file formats to be .dcm files
 
+% Prompt user to choose a library with dicom files
 dirName = uigetdir('ExamDICOMdata','Choose a library with dicom files');
+
+% Check if the library contains data
+% and load images and info into the output
 if dirName ~= 0
     files = dir(fullfile(dirName, '*.dcm'));
-    num_files = length(files);
+    numFiles = length(files);
     wb = waitbar(0,'Loading dicom files...');
-    for i = 1:num_files
-    Info = dicominfo(fullfile(dirName,files(i).name)); 
-    handles.MyData.Images = dicomread(fullfile(dirName,files(i).name));
-    handles.MyData.Info = Info;
+    % Diable GUI fields when loading images
+    % handles = disableFields(handles);
+    for ii=1:numFiles
+    Info = dicominfo(fullfile(dirName,files(ii).name));
+    MyData.Image = dicomread(fullfile(dirName,files(ii).name));
+    MyData.Info = Info;
+    MyData.EchoTime = Info.EchoTime; 
+    MyData.sliceLocation = Info.SliceLocation;
     end
-    waitbar(i/num_files,wb);
+    waitbar(ii/numFiles,wb);
 end
 
 if exist('wb','var')
     close(wb);
 end
 
-numDICOM = length(MyData.DICOM);
-set(handles.sldImage,'Max',numDICOM);
-set(handles.sldImage,'Value',1);
-set(handles.sldImage,'SliderStep',[1/(numDICOM-1) 5/(numDICOM-1)]);
+% Save number of images and setup slider
+numDICOM = length(MyData.Image);
+set(handles.sldImages,'Max',numDICOM);
+set(handles.sldImages,'Value',1);
+set(handles.sldImages,'SliderStep',[1/(numDICOM-1) 5/(numDICOM-1)]);
 
+% Set text to slider to numbers of images
 set(handles.txtNum,'String',sprintf('1/%d',numDICOM));
 
 end
