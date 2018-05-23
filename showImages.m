e7a3442ec6b8330ccf3ff1829d2b49ecb728ce39
cttltran <23510284+cttltran@users.noreply.github.com>
2018-05-23 12:10:03 +0200

    updated function

--- showImages.m
+++ showImages.m
@@ -1,8 +1,8 @@
 function showImages(handles)
 %SHOWIMAGES The function displays images from loaded dicom files
-%   
+%   The function also gives the opportunity to edit contrast
 
-% Read the position of the image of the slider
+% Read the position of the slider
 ImagePos = round(get(handles.sldImages,'Value'));
 
 % Setup text for the slider and show loaded images
@@ -10,10 +10,5 @@ set(handles.txtNum,'String',sprintf('%i/%i',ImagePos,length(handles.MyData.Dicom
 axes(handles.axImage);
 imshow(handles.MyData.Dicom(ImagePos).Image);
 axis equal off;
-
-% Setup contrast for the images
-% set(gca,'clim',[0 50]);
-
-
 end
 
