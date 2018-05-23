d086fed311baa892758e98d80793393e194ccb4b
cttltran <23510284+cttltran@users.noreply.github.com>
2018-05-23 23:29:04 +0200

    updated function

--- showImages.m
+++ showImages.m
@@ -6,9 +6,21 @@ function showImages(handles)
 ImagePos = round(get(handles.sldImages,'Value'));
 
 % Setup text for the slider and show loaded images
-set(handles.txtNum,'String',sprintf('%i/%i',ImagePos,length(handles.MyData.Dicom)));
+set(handles.txtNum,'String',...
+    sprintf('%i/%i',ImagePos,length(handles.MyData.Dicom)));
 axes(handles.axImage);
 imshow(handles.MyData.Dicom(ImagePos).Image);
 axis equal off;
+
+if isfield(handles.MyData,'ROIpos')
+   handles.MyData.ROIpos = drawROI(handles);
+   guidata(handles.btnZoom,handles);
+end
+
+if isfield(handles.MyData,'XLim')
+   set(handles.axImage,'XLim',handles.MyData.XLim);
+   set(handles.axImage,'YLim',handles.MyData.YLim);
+end
+
 end
 
