b4e637cc43cb0ed8e2a11472a08af5f7d6e2fb21
cttltran <23510284+cttltran@users.noreply.github.com>
2018-05-24 17:22:13 +0200

    Update main.m

--- main.m
+++ main.m
@@ -22,7 +22,7 @@ function varargout = main(varargin)
 
 % Edit the above text to modify the response to help main
 
-% Last Modified by GUIDE v2.5 23-May-2018 14:25:32
+% Last Modified by GUIDE v2.5 24-May-2018 11:04:27
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -207,3 +207,26 @@ function btnZoom_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 handles = zoomImage(handles);
 guidata(hObject,handles);
+
+
+% --- Executes on selection change in menuExclude.
+function menuExclude_Callback(hObject, eventdata, handles)
+% hObject    handle to menuExclude (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns menuExclude contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from menuExclude
+
+
+% --- Executes during object creation, after setting all properties.
+function menuExclude_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to menuExclude (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
