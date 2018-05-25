13913f5d0bc7cbe6e5722bd58750c93c843d9c21
cttltran <23510284+cttltran@users.noreply.github.com>
2018-05-25 12:06:32 +0200

    Add files via upload

---
+++ analyseROI.m
@@ -0,0 +1,33 @@
+function calcROI( handles, ROI, pos )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+ROIvertices = getVertices(ROI);
+
+axes(handles.axImage);
+    
+h = findobj(handles.axImage,'Tag','ROI');
+
+    if isempty(h)
+       rectangle('Position',pos,'Curvature',[1 1],'EdgeColor','b','Tag','ROI');
+    else
+       set(h,'Position',pos);
+    end
+
+% aflæs position (hvilket billede) for slider
+ImPosition = round(get(handles.sldImages,'Value'));
+w = size(handles.MyData.imgT2(ImPosition).Image,1);
+h = size(handles.MyData.imgT2(ImPosition).Image,2);
+bw = poly2mask(ROIvertices(:,1),ROIvertices(:,2),w,h);
+handles.MyData.ROImean = mean(handles.MyData.imgT2(ImPosition).Image(bw));
+% Temporary display of mean 
+set(handles.txtT2_1,'String',...
+    sprintf('%d',handles.MyData.ROImean));
+
+%Vel = zeros(length(handles.MyData.Dicom));
+    %for i=1:length(handles.MyData.Dicom)
+        %currentDicom = handles.MyData.Dicom(i).Image;
+        %Vel(i)=mean(currentDicom(bw()));
+    %end
+    
+end
